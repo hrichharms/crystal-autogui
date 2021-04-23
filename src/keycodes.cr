ef2600adb661dc6beaa9a5a9fa58b733cdaa18d0
Heinrich <freelanceharms@gmail.com>
2021-04-23 11:07:12 -0500

    alphanumerical charact keycodes

---
+++ src/keycodes.cr
@@ -0,0 +1,41 @@
+keycodes = Hash(Char, Int32).new
+
+# numbers
+keycodes['1'] = 10
+keycodes['2'] = 11
+keycodes['3'] = 12
+keycodes['4'] = 13
+keycodes['5'] = 14
+keycodes['6'] = 15
+keycodes['7'] = 16
+keycodes['8'] = 17
+keycodes['9'] = 18
+keycodes['0'] = 19
+
+# alphabet
+keycodes['A'] = 38
+keycodes['B'] = 56
+keycodes['C'] = 54
+keycodes['D'] = 40
+keycodes['E'] = 26
+keycodes['F'] = 41
+keycodes['G'] = 42
+keycodes['H'] = 43
+keycodes['I'] = 31
+keycodes['J'] = 44
+keycodes['K'] = 45
+keycodes['L'] = 46
+keycodes['M'] = 58
+keycodes['N'] = 57
+keycodes['O'] = 32
+keycodes['P'] = 33
+keycodes['Q'] = 24
+keycodes['R'] = 27
+keycodes['S'] = 39
+keycodes['T'] = 28
+keycodes['U'] = 30
+keycodes['V'] = 55
+keycodes['W'] = 25
+keycodes['X'] = 53
+keycodes['Y'] = 29
+keycodes['Z'] = 52
